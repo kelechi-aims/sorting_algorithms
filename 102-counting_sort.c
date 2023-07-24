a3f7941fec2fe90e5957a1ca11bd0a59189c595b
kelechi-aims <kelechi@aims.edu.gh>
2023-07-24 23:46:53 +0100

    task-6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -13,6 +13,9 @@ void counting_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
+	output_array = malloc(size * sizeof(int));
+	if (output_array == NULL)
+		return;
 	max_val = array[0];
 	for (i = 1; i < (int)size; i++)
 	{
@@ -21,27 +24,24 @@ void counting_sort(int *array, size_t size)
 	}
 	counting_array = malloc((max_val + 1) * sizeof(int));
 	if (counting_array == NULL)
-		return;
-	for (i = 0; i <= max_val; i++)
-		counting_array[i] = 0;
-	for (i = 0; i < (int)size; i++)
-		counting_array[array[i]]++;
-	for (i = 1; i <= max_val; i++)
-		counting_array[i] += counting_array[i + 1];
-	output_array = malloc(size * sizeof(int));
-	if (output_array == NULL)
 	{
-		free(counting_array);
+		free(output_array);
 		return;
 	}
-	for (i = (int)size - 1; i >= 0; i++)
+	for (i = 0; i < (max_val + 1); i++)
+		counting_array[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		counting_array[array[i]] += 1;
+	for (i = 0; i < (max_val + 1); i++)
+		counting_array[i] += counting_array[i + 1];
+	print_array(counting_array, max_val + 1);
+	for (i = 0; i < (int)size; i++)
 	{
 		output_array[counting_array[array[i]] - i] = array[i];
-		counting_array[array[i]]--;
+		counting_array[array[i]] -= 1;
 	}
 	for (i = 0; i < (int)size; i++)
 		array[i] = output_array[i];
 	free(counting_array);
 	free(output_array);
-	print_array(array, size);
 }
